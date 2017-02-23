896fc6f1e93d44c984ac341f8de5057d087c9021
Sebastian Spreizer <spreizer@web.de>
2017-02-23 14:29:40 +0100

    170223-2

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,13 +6,15 @@ const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-adapter-idb'));
 PouchDB.plugin(require('pouchdb-upsert'));
 const uuidV4 = require('uuid/v4');
+const fs = require('fs')
+const path = require('path')
 
 var db = {};
 
 db.init = function() {
     var config = app.config.app()
     var db_name = config.get('db.name')
-    var filename = config.get('db.local.path') + '/' + db_name + '.db'
+    var filename = path.join(config.get('db.local.path'), db_name + '.db')
 
     db.indexDB = new PouchDB(db_name, {
         adapter: 'idb'
@@ -30,8 +32,18 @@ db.init = function() {
     // })
     db.localDB = new NeDB({
         filename: filename,
-        autoload: true,
         timestampData: true,
+    })
+
+    db.localDB.loadDatabase(function(err) {
+        var imagePath = path.join(__dirname, '..', '..', config.get('db.local.path'), 'images');
+        var exportPath = path.join(__dirname, '..', '..', config.get('db.local.path'), 'exports');
+        if (!fs.existsSync(imagePath)) {
+            fs.mkdirSync(imagePath)
+        }
+        if (!fs.existsSync(exportPath)) {
+            fs.mkdirSync(exportPath)
+        }
     });
 
     app.sync.on()
@@ -82,7 +94,7 @@ db.export = function(id) {
     const config = app.config.app()
     db.get(id).exec(function(err, doc) {
         if (err) return
-        var filepath = __dirname + '/../../' + config.get('db.local.path') + '/exports/' + id + '.json'
+        var filepath = path.join(__dirname, '..', '..', config.get('db.local.path'), 'exports', id + '.json')
         jsonfile.writeFileSync(filepath, doc, {
             spaces: 4
         })
