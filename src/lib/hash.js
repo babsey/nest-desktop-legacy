239a6968f2e4baced7fc53313a93e17f9fa65515
Sebastian Spreizer <spreizer@web.de>
2017-04-28 11:09:31 +0200

    170429-2

--- src/lib/hash.js
+++ src/lib/hash.js
@@ -4,6 +4,10 @@ const hash = require('object-hash');
 
 module.exports = function(data) {
     return hash({
+        name: data.name,
+        description: (data.description || ""),
+        network: data.network,
+        kernel: data.kernel,
         nodes: data.nodes.map(function(node) {
             return {
                 model: node.model,
