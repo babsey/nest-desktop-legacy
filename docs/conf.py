7622a2c449332628ca18965b65c881d0acffabc1
Sebastian Spreizer <spreizer@web.de>
2019-09-21 13:14:00 +0200

    latest version

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,8 @@ author = 'Sebastian Spreizer'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+# version = '2.0'
+version = 'latest'
 # The full version, including alpha/beta/rc tags.
 release = '2.0'
 
