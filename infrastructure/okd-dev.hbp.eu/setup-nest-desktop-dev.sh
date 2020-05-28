f82b891362ae147c4f2d9af56043c8b4f9e72053
Sebastian Spreizer <spreizer@web.de>
2020-05-28 11:17:59 +0200

    Update setup for okd-dev

--- infrastructure/okd-dev.hbp.eu/setup-nest-desktop-dev.sh
+++ infrastructure/okd-dev.hbp.eu/setup-nest-desktop-dev.sh
@@ -6,7 +6,6 @@ oc project nest-desktop
 oc delete is,dc,svc,configMap,route nest-desktop nest-desktop-apache2oidc
 
 oc new-app babsey/nest-desktop:master+babsey/nest-desktop-apache2oidc:latest
-oc new-app nest-desktop-dev.yaml
 oc new-app nest-desktop-apache2oidc.yaml
 oc set env --from='configmap/nest-desktop-apache2oidc' dc/nest-desktop
 oc create route edge --hostname='nest-desktop.apps-dev.hbp.eu' --port='8080-tcp' --service='nest-desktop'
