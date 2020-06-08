c4d349a5beb5a4ca3967b05823424072549cd086
Sebastian Spreizer <spreizer@web.de>
2020-06-08 09:33:33 +0200

    Remove toolbox functions in simulation component

--- src/app/simulation/simulation.component.ts
+++ src/app/simulation/simulation.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { MatMenuTrigger } from '@angular/material/menu';
 
 import { AnimationControllerService } from '../visualization/animation/animation-controller/animation-controller.service';
-import { AppService } from '../app.service';
-import { ColorService } from '../network/services/color.service';
 import { DataService } from '../services/data/data.service';
 import { NetworkService } from '../network/services/network.service';
-import { VisualizationService } from '../visualization/visualization.service';
-import { SimulationControllerService } from './simulation-controller/simulation-controller.service';
-import { SimulationProtocolService } from './services/simulation-protocol.service';
 import { SimulationEventService } from './services/simulation-event.service';
 import { SimulationRunService } from './services/simulation-run.service';
 import { SimulationService } from './services/simulation.service';
@@ -29,24 +23,16 @@ import { enterAnimation } from '../animations/enter-animation';
 })
 export class SimulationComponent implements OnInit, OnDestroy {
 
-  @ViewChild(MatMenuTrigger, { static: false }) contextMenu: MatMenuTrigger;
-  contextMenuPosition = { x: '0px', y: '0px' };
-
   constructor(
     private _animationControllerService: AnimationControllerService,
-    private _appService: AppService,
-    private _colorService: ColorService,
     private _dataService: DataService,
     private _networkService: NetworkService,
-    private _simulationProtocolService: SimulationProtocolService,
     private bottomSheet: MatBottomSheet,
     private route: ActivatedRoute,
     private router: Router,
-    public _simulationControllerService: SimulationControllerService,
     public _simulationEventService: SimulationEventService,
     public _simulationRunService: SimulationRunService,
     public _simulationService: SimulationService,
-    public _visualizationService: VisualizationService,
   ) {
   }
 
@@ -84,50 +70,18 @@ export class SimulationComponent implements OnInit, OnDestroy {
 
   run(force: boolean = false): void {
     this._simulationService.mode = 'activityExplorer';
+    this._animationControllerService.stop();
+    if (this._networkService.recorderChanged) {
+      this._simulationEventService.records = [];
+      this._networkService.recorderChanged = false;
+    }
     this._networkService.clean(this._simulationService.data);
     this._simulationRunService.run(this._simulationService.data, force)
-  }
-
-  hasRecords(): boolean {
-    return this._simulationEventService.records.length > 0;
-  }
-
-  download(): void {
-    var data = this._dataService.clean(this._simulationService.data);
-    if (this._simulationEventService.records.length > 0) {
-      data['records'] = this._simulationEventService.records;
-    }
-    this._simulationProtocolService.download([data])
-  }
-
-  configSimulation(): void {
-    this._simulationService.mode = 'activityExplorer';
-    this._simulationControllerService.mode = 'simulation';
   }
 
   onDataChange(data: Data): void {
     // console.log('Simulation on data change')
     this._simulationService.data._id = '';
-  }
-
-  onSelectionChange(event: any): void {
-    this._simulationRunService.config[event.option.value] = event.option.selected;
-    this._simulationRunService.saveConfig()
-  }
-
-  onMouseOver(event: MouseEvent): void {
-    this._appService.rightClick = true;
-  }
-
-  onMouseOut(event: MouseEvent): void {
-    this._appService.rightClick = false;
-  }
-
-  onContextMenu(event: MouseEvent): void {
-    event.preventDefault();
-    this.contextMenuPosition.x = event.clientX + 'px';
-    this.contextMenuPosition.y = event.clientY + 'px';
-    this.contextMenu.openMenu();
   }
 
 }
